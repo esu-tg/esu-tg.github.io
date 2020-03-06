f086f14a0c3d09c444e30d377707d322ef3d9670
esu-tg <59681123+esu-tg@users.noreply.github.com>
2020-03-06 14:41:50 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 
 function go(){
-alert('test success');
+  alert('test success');
+  window.location.href="tg://proxy?server=54.255.152.218&port=443&secret=ee000000000000000000000000000000017777772e62696e672e636f6d"; 
 }
